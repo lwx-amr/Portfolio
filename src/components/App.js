f70e876091e382d5034ff7e1b64898ef51425f4c
Amr.H <amr.ister20@gmail.com>
2020-09-25 17:34:31 +0200

    Project base

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="app bg-pink-900 text-white h-screen">
+      <header className="App-header text-center pt-20">
+        <h1 className="text-3xl">Hello This is first base</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  important: true,
+  theme: {
+    fontFamily: {
+      sans: ['Work Sans', 'sans-serif']
+    }
+  }
+}
